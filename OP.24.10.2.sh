08bad0c083d1aa09af2a4931b2ff609baa194524
Albert <swiftfrog@gmail.com>
2025-11-25 20:33:02 +0800

    Update OP.24.10.2.sh

--- OP.24.10.2.sh
+++ OP.24.10.2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.9.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.8.0.1/g' package/base-files/files/bin/config_generate
 
 #  modify NO.
 # 23.05.2
